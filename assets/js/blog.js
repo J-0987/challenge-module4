55a98babe22ac7de4b04270f37ab6c5662cb82db
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-03-18 18:40:27 -0400

    fixed change theme box not working on blog page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,13 +1,59 @@
+
+// access button to change theme
+const changeTheme= document.querySelector('#change-theme');
+const container = document.querySelector('.container');
+const headerTag = document.querySelector('header');
+const header = document.querySelector('h2');
+const backLink = document.querySelector('a');
+
+//Gets user input from localDtorage and parses it into js
 let logEntry = JSON.parse (localStorage.getItem('logData')) || [];
+
 var HTMLcode = ""
+
+//loop to input user content into blog page
 
 for (let i=0; i <logEntry.length; i++) {
 HTMLcode += `<div class="blog-title">
-    <h2>${logEntry[i].title}</h2>
+    <h3>${logEntry[i].title}</h3>
     <p> ${logEntry[i].content} </p>
-    <h6> ${logEntry[i].Username} </h6>
+    <h6> ${`By:`} ${logEntry[i].Username} </h6>
 </div>`
 
 }
 let blogEntry = document.querySelector('#blog-entry');
 blogEntry.innerHTML = HTMLcode;
+
+
+
+
+
+// Set default mode to light
+let mode = 'light';
+
+// Listen for a click event on toggle switch
+changeTheme.addEventListener('click', function () {
+  // If mode is dark, apply light background
+  if (mode === 'light') {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+    header.classList.remove ('header-light');
+    header.classList.add ('header-dark');
+    headerTag.classList.remove('header-light');
+    headerTag.classList.add('header-dark');
+    backLink.classList.remove ('header-light');
+    backLink.classList.add ('header-dark');
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+    header.classList.remove ('header-dark');
+    header.classList.add ('header-light');
+    headerTag.classList.remove('header-dark');
+    headerTag.classList.add('header-light');
+    backLink.classList.add ('header-light');
+    backLink.classList.remove ('header-dark');
+
+  }
+});
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,6 +3,7 @@ const changeTheme= document.querySelector('#change-theme');
 const container = document.querySelector('.container');
 const headerTag = document.querySelector('header');
 const header = document.querySelector('h2');
+const formMainPage = document.querySelector('.form-mainpage');
 
 
 
@@ -32,3 +33,15 @@ changeTheme.addEventListener('click', function () {
   }
 });
 
+formMainPage.addEventListener ('submit', function (event) {
+  event.preventDefault ();
+  const submitData = {
+    username: document.getElementById ('Username').value,
+    title: document.getElementById ('title').value,
+    content: document.getElementById ('content').value,
+  }
+  let logEntry = JSON.parse (localStorage.getItem('logData')) || []
+  logEntry.push(submitData);
+  localStorage.setItem("logData",JSON.stringify (logEntry));
+  location.replace("./blog.html")
+})

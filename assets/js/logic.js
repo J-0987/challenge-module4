ac3767d5574f409cf3ee686c1669c36e89d15a50
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-03-19 12:22:31 -0400

    continued styling of blog page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,5 @@
 // access button to change theme
-const changeTheme= document.querySelector('#change-theme');
+const changeTheme = document.querySelector('#change-theme');
 const container = document.querySelector('.container');
 const headerTag = document.querySelector('header');
 const header = document.querySelector('h2');
@@ -16,8 +16,8 @@ changeTheme.addEventListener('click', function () {
   if (mode === 'light') {
     mode = 'dark';
     container.setAttribute('class', 'dark');
-    header.classList.remove ('header-light');
-    header.classList.add ('header-dark');
+    header.classList.remove('header-light');
+    header.classList.add('header-dark');
     headerTag.classList.remove('header-light');
     headerTag.classList.add('header-dark');
   }
@@ -25,23 +25,40 @@ changeTheme.addEventListener('click', function () {
   else {
     mode = 'light';
     container.setAttribute('class', 'light');
-    header.classList.remove ('header-dark');
-    header.classList.add ('header-light');
+    header.classList.remove('header-dark');
+    header.classList.add('header-light');
     headerTag.classList.remove('header-dark');
     headerTag.classList.add('header-light');
 
   }
 });
 
-formMainPage.addEventListener ('submit', function (event) {
-  event.preventDefault ();
+formMainPage.addEventListener('submit', function (event) {
+  event.preventDefault();
   const submitData = {
-    username: document.getElementById ('Username').value,
-    title: document.getElementById ('title').value,
-    content: document.getElementById ('content').value,
+    username: document.getElementById('username').value,
+    title: document.getElementById('title').value,
+    content: document.getElementById('content').value,
   }
-  let logEntry = JSON.parse (localStorage.getItem('logData')) || []
+
+  let logEntry = JSON.parse(localStorage.getItem('logData')) || []
+
+  //Method 2 to alert user. if statement for each field. Method 1 is adding "required" to each line in HTML form. Both work.
+  if (submitData.username === "") {
+    alert ("Username field cannot be empty!");
+    return false; 
+  }
+
+  if (submitData.title === "") {
+    alert ("Title field cannot be empty!");
+    return false; 
+  }
+  if (submitData.content === "") {
+    alert ("Content field cannot be empty!");
+    return false; 
+  }
+
   logEntry.push(submitData);
-  localStorage.setItem("logData",JSON.stringify (logEntry));
+  localStorage.setItem("logData", JSON.stringify(logEntry));
   location.replace("./blog.html")
-})
+});
